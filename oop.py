b69538ed687258492fae78131f383d3132224e99
2manycooks <73556731+2manycooks@users.noreply.github.com>
2023-08-04 15:10:07 -0700

    lesson done

--- oop.py
+++ oop.py
@@ -36,4 +36,60 @@ class Car():
 
 gabes_tesla = Car("Tesla", "Y", "1950")
 
-print(gabes_tesla)
+
+# <-------- INHERITANCE -------->
+
+class Phone():
+    def __init__(self, number):
+        self.number = number
+
+    def __str__(self):
+        return f'you have called {self.number}'
+
+    def call(self, other_number):
+        print(f'{self.number} is calling {other_number}')
+
+    def text(self, other_number):
+        print(f'{self.number} is texting {other_number}')
+
+gabes_phone = Phone("6661234")
+
+
+
+
+
+
+class iPhone(Phone):
+    def __init__(self, number, generation, color, fingerprint = None):
+        super().__init__(number)
+        self.generation = generation
+        self.color = color
+        self.fingerprint = fingerprint
+
+    def __str__(self):
+        return f'ooh look at me, i have an iphone{self.generation} and its {self.color}'
+    
+    def set_fingerprint(self, digit):
+        self.fingerprint = digit
+
+    def unlock(self, input):
+        if (self.fingerprint == input):
+            print("welcome, phone unlocked!")
+        else:
+            print("invalid input, calling the cia")
+
+gabes_iphone = iPhone("6661234", "14", "rose gold")
+
+gabes_iphone.set_fingerprint("thumb")
+
+class Android(Phone):
+    def __init__(self, number, keyboard = "Default"):
+        super().__init__(number)
+        self.keyboard = keyboard
+
+    def __str__(self):
+        return f'This Android uses the {self.keyboard} keyboard.'
+    
+gabes_pixel = Android("6661234")
+
+print(gabes_pixel)
